bd31834f0182f1a28c61d055652c568a936a9e1a
Parag Dadhaniya <paragdadhaniya@gmail.com>
2014-07-28 12:39:22 -0500

    add methods and attributes to person class

--- powerranger.rb
+++ powerranger.rb
@@ -1,4 +1,25 @@
+
 class Person
+
+	def initialize(name, caffeine_level=0)
+		@name = name
+		@caffeine_level = caffeine_level
+	end
+
+	def run
+		puts "You are running away from your enemies! Go back and fight!"
+	end
+
+	def scream
+		puts "Ahhh!! save me!!!!"
+	end
+
+	def drink_coffee
+		puts "That was a refreshing cup of joe!"
+		@caffeine_level += 1
+		puts "Your new caffeine level is #{@caffeine_level}."
+	end
+
 end
 
 class PowerRanger
